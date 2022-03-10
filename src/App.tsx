86b18848dcbcc0138417fd55623d7bc85915c3c3
Alexander Davydov <aleksandrd@wix.com>
2022-03-10 17:32:06 +0200

    feat: add xstate changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useMachine, useSelector } from "@xstate/react";
+import React from "react";
+import { assign, createMachine } from "xstate";
+
+interface ToggleMachineContext {
+  count: number;
+}
+
+interface ToggleEvent {
+  type: "TOGGLE";
+}
+
+type ToggleMachineEvent = ToggleEvent;
+
+const toggleMachine = createMachine<ToggleMachineContext, ToggleMachineEvent>({
+  id: "toggle",
+  initial: "inactive",
+  context: {
+    count: 0,
+  },
+  states: {
+    inactive: {
+      on: { TOGGLE: "active" },
+    },
+    active: {
+      entry: assign({ count: (ctx) => ctx.count + 1 }),
+      on: { TOGGLE: "inactive" },
+    },
+  },
+});
 
 function App() {
+  const [state, send, service] = useMachine(toggleMachine);
+
+  const active = state.matches("active");
+
+  const countFromSelector = useSelector(
+    service,
+    ({ context }) => context.count
+  );
+  console.log("countFromSelector", countFromSelector);
+
+  const count = state.context.count;
+  console.log("count", count);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>XState React Template</h1>
+      <h2>Fork this template!</h2>
+      <button onClick={() => send("TOGGLE")}>
+        Click me ({active ? "✅" : "❌"})
+      </button>{" "}
+      <code>
+        Toggled <strong>{count}</strong> times
+      </code>
     </div>
   );
 }
